cb84190a800869a3e1e38ba0786d4ed225691658
jake <jakemercure28@gmail.com>
2021-02-06 22:57:45 -0800

    menu

---
+++ game.cpp
@@ -0,0 +1,30 @@
+//
+// Created by jake on 2/3/21.
+//
+#include "game.h"
+
+game::game(){
+
+    username = "";
+    score = 0;
+}
+
+void game::display_rules(){
+    cout << "Insert summary here.";
+}
+
+void game::play_game() {
+
+}
+
+void game::load_previous_game() {
+
+}
+
+void game::add_command() {
+
+}
+
+void game::remove_command() {
+
+}
