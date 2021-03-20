dfd072524f61c9945cbbfdcc458e71e3f2b818ea
jakemercure28 <jakemercure28@gmail.com>
2021-03-20 10:59:50 -0700

    .h file implemented

---
+++ AVLnode.h
@@ -0,0 +1,19 @@
+//
+// Created by jake on 3/20/21.
+//
+
+#ifndef CPTS_223_AVLNODE_H
+#define CPTS_223_AVLNODE_H
+
+
+// create template too
+class AVLnode
+{
+public:
+    int data;
+    AVLnode *left;
+    AVLnode *right;
+    int height;
+};
+
+#endif //CPTS_223_AVLNODE_H
