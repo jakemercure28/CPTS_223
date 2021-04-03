726a4b7d56a3aa0ab91f42ba8dc7c5e363b44a33
jakemercure28 <jakemercure28@gmail.com>
2021-04-03 12:07:14 -0700

    Both searches work

--- main.cpp
+++ main.cpp
@@ -20,24 +20,62 @@ int main(int argc, char* argv[])
         twitter_data[i].setCategory(raw_data[i][5]);
     }
 
+    /****************
+     * Map scenario 1
+     ****************/
+
+    //inserting into map Key: integer, Data: TwitterData data type
     map <int, TwitterData> m1;
-    for (int i =0; i < 5;i++) {
+    for (int i =0; i < 5;i++)
             m1.insert(pair<int, TwitterData>(i , twitter_data[i]));
-    }
 
+    //printing map
     map<int, TwitterData>::iterator itr;
-    for(itr = m1.begin();itr != m1.end();itr++){
-        cout << itr->first << '\t' << itr->second.getUserName() << '\n';
-        cout << itr->first << '\t' << itr->second.getActualName() << '\n';
-        cout << itr->first << '\t' << itr->second.getEmail() << '\n';
-        cout << itr->first << '\t' << itr->second.getNumTweets() << '\n';
-        cout << itr->first << '\t' << itr->second.getCategory() << '\n';
-    }
+    for(itr = m1.begin();itr != m1.end();itr++)
+        cout << itr->first << '\t' << itr->second.print() << '\n';
 
+    //searching for username and erasing record
     cout << "Searching for Name...\n";
     for(itr = m1.begin();itr != m1.end();itr++)
-        if(itr->second.getUserName() == "savage1")
+        if(itr->second.getUserName() == "savage1") {
             cout << "Found: " << itr->second.print() << endl;
+            cout << "Removing this person from map...\n";
+            m1.erase(itr);
+        }
+
+    //checking if user was erased
+    for(itr = m1.begin();itr != m1.end();itr++)
+        cout << itr->first << '\t' << itr->second.print() << '\n';
+
+    /****************
+     * Map scenario 2
+     ****************/
+
+    cout << "\nMap scenario 2\n\n";
+    //parameters: int, Twitter data class datatype
+    cout << "Inserting into map...\n";
+    map<int, TwitterData> m2;
+    for (int i =0; i < 5;i++)
+        m2.insert(pair<int, TwitterData>(i , twitter_data[i]));
+
+    //printing map
+    cout << "Printing map...\n";
+    for(itr = m2.begin();itr != m2.end();itr++)
+        cout << itr->first << '\t' << itr->second.print() << '\n';
+
+    //searching for email and erasing record
+    cout << "Searching for Email...\n";
+    for(itr = m2.begin();itr != m2.end();itr++)
+        if(itr->second.getEmail() == "kat@gmail.com") {
+            cout << "Found: " << itr->second.print() << endl;
+            cout << "Removing this person from map...\n";
+            m2.erase(itr);
+        }
+
+    //checking if user was erased
+    cout << "User erased...\n";
+    for(itr = m2.begin();itr != m2.end();itr++)
+        cout << itr->first << '\t' << itr->second.print() << '\n';
 
     return 0;
 }
